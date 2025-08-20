4bf805174c98158187632abb51c3e6bf33ebb244
amirhoseinheydari <amirhosein.hydri1381@email.com>
2025-08-20 21:59:11 +0330

    add test

--- core/blog/tests.py
+++ core/blog/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from .views import PostViewset
 
-# Create your tests here.
+
+class TestUrl(TestCase):
+
+    def test_blog_index_url_resolve(self):
+        url = reverse('blog:index')
+        self.assertEqual(resolve(url).func)
+
